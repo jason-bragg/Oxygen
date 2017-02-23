bcc874b7f04b293f9d70051307e18e60592acb05
jason-bragg <jbragg@microsoft.com>
2017-02-22 23:49:46 -0800

    ggg

---
+++ Orleans/src/Microsoft.Orleans.Facet.Abstractions/TypeFacetInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Orleans.Facet.Abstractions
+{
+    public class TypeFacetInfo
+    {
+
+        public TypeFacetInfo(Type type)
+        {
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            var setters = fields.Where(fi => typeof(IGrainFacet).IsAssignableFrom(fi.FieldType)).Select(fi => MakeFieldSetter(fi))
+        }
+
+        Action<object, IServiceProvider> MakeFieldSetter(FieldInfo fieldInfo)
+        {
+            
+        }
+    }
+}
---
+++ Orleans/src/Microsoft.Orleans.Factory.Abstractions/FactoryType.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Microsoft.Orleans.Factory.Abstractions
+{
+    public static class FactoryType
+    {
+        public static Type CreateFactoryInterfaceType(Type ttype)
+        {
+            Type[] typeArgs = { ttype };
+            return typeof(IFactory<>).MakeGenericType(typeArgs);
+        }
+        public static Type CreateFactoryInterfaceType(Type tkey, Type ttype)
+        {
+            Type[] typeArgs = { tkey, ttype };
+            return typeof(IFactory<,>).MakeGenericType(typeArgs);
+        }
+    }
+}
